3031cff03a81b25b12ca71ddfcf846ab0a93db1c
Alperen Avan <alperen_0150@hotmail.com>
2024-07-05 13:27:00 +0300

    Control External LED

--- 1-Control External LED/sketch_jul3a/sketch_jul3a.ino
+++ 1-Control External LED/sketch_jul3a/sketch_jul3a.ino
@@ -3,14 +3,14 @@ void setup() {
   pinMode(13,OUTPUT);
   digitalWrite(13,HIGH);
 
-  pinMode(6,OUTPUT);
-  digitalWrite(6,HIGH);
+  pinMode(7,OUTPUT);
+  digitalWrite(7,HIGH);
 
   delay(4000);
 
-  digitalWrite(6,LOW);
-  pinMode(5,OUTPUT);
-  digitalWrite(5,HIGH);
+  digitalWrite(7,LOW);
+  pinMode(6,OUTPUT);
+  digitalWrite(6,HIGH);
 
 
 }
